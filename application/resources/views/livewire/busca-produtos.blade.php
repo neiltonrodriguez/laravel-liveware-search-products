bb2b0aa87fa31511ca48d9d455fd1eaa532377e4
neiltonrodriguez <neiltonrodriguez@gmail.com>
2025-07-18 14:13:13 -0300

    mudando nome do botão de pesquisa

--- application/resources/views/livewire/busca-produtos.blade.php
+++ application/resources/views/livewire/busca-produtos.blade.php
@@ -45,7 +45,7 @@
                 <button
                     wire:click="buscar"
                     class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md shadow-lg transition-colors duration-200">
-                    Pesquisar Produtos
+                    Aplicar Filtros
                 </button>
                 <button
                     wire:click="limpar"
